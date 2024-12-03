89682c842cc96ccf17cec7655064715f57b7c416
Simon Samuel <salviensky@gmail.com>
2024-12-03 15:24:49 +0100

    chore: set up hardhat feat: added agence contract chore: added boilerplate contracts

---
+++ contracts/ethereum/src/Agence.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+contract Agence {
+    address public immutable governanceContract;
+    address public immutable stakingContract;
+    address public immutable gigsContract;
+
+    constructor(
+        address _governanceContract,
+        address _stakingContract,
+        address _gigsContract
+    ) {
+        governanceContract = _governanceContract;
+        stakingContract = _stakingContract;
+        gigsContract = _gigsContract;
+    }
+
+    function getGovernance() external view returns (address) {
+        return governanceContract;
+    }
+
+    function getStaking() external view returns (address) {
+        return stakingContract;
+    }
+
+    function getGigs() external view returns (address) {
+        return gigsContract;
+    }
+}
---
+++ contracts/ethereum/test/Agence.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Agence} from "../src/Agence.sol";
+
+contract AgenceTest is Test {
+    Agence public agence;
+
+    function setUp() public {}
+}
