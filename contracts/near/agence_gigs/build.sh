05e2e86b9eec2ab48fa4bb77004bfc9cf9c10f87
Simon Samuel <salviensky@gmail.com>
2024-12-07 20:52:12 +0100

    chore: removing algorand and near contracts

--- contracts/near/agence_gigs/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-cargo near build --no-docker
-mkdir -p ./out
-
-cp ../target/near/agence_gigs/agence_gigs.wasm ./out/gigs_contract.wasm
-cp ../target/near/agence_gigs/agence_gigs_abi.json ./out/gigs_contract_abi.json
--- contracts/near/agence_governance/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-cargo near build --no-docker
-mkdir -p ./out
-
-cp ../target/near/agence_governance/agence_governance.wasm ./out/governance_contract.wasm
-cp ../target/near/agence_governance/agence_governance_abi.json ./out/governance_contract_abi.json
