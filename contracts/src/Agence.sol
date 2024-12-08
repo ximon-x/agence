c7b4c98801dfa80fdf202b7669b0835b7c79579c
Simon Samuel <salviensky@gmail.com>
2024-12-08 06:01:00 +0100

    chore: added smart contracts

---
+++ contracts/src/Agence.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {AgenceGovernor} from "./AgenceGovernor.sol";
+import {AgenceTreasury} from "./AgenceTreasury.sol";
+import {AgenceGigs} from "./AgenceGigs.sol";
+
+import {AgenceToken} from "./AgenceToken.sol";
+import {sUSDe} from "./mocks/sUSDe.sol";
+import {USDe} from "./mocks/USDe.sol";
+
+contract Agence {
+    AgenceGovernor public immutable governor;
+    AgenceTreasury public immutable treasury;
+    AgenceGigs public immutable gigs;
+
+    AgenceToken public immutable votingToken;
+    sUSDe public immutable rewardsToken;
+    USDe public immutable stakingToken;
+
+    uint256 constant INITIAL_SUPPLY = 1000 ether;
+
+    constructor() {
+        votingToken = new AgenceToken(INITIAL_SUPPLY, msg.sender);
+        rewardsToken = new sUSDe(INITIAL_SUPPLY, msg.sender);
+        stakingToken = new USDe(INITIAL_SUPPLY, msg.sender);
+
+        // governor = new AgenceGovernor(msg.sender);
+
+        treasury = new AgenceTreasury(
+            msg.sender,
+            address(rewardsToken),
+            address(stakingToken)
+        );
+
+        gigs = new AgenceGigs(msg.sender);
+    }
+}
